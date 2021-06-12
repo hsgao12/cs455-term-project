21a779d4f1be19558201040019ddf7f8a59f8409
jungwookjang <b8n2b@ugrad.cs.ubc.ca>
2021-06-11 17:48:26 -0700

    ShoesListing initial commits

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import ReleasesGrid from './components/upcoming-releases/ReleasesCarousel';
 import Login from './components/login/Login'
 import {Modal} from "@material-ui/core";
+import ShoesList from "./components/shoesListing/ShoesList";
 
 function App() {
     const [loginFormOpen,setLoginFormOpen] = useState(false);
@@ -9,6 +10,7 @@ function App() {
     return (
         <div className="App">
             <ReleasesGrid/>
+            <ShoesList/>
             <Modal open={loginFormOpen} onClose={()=>setLoginFormOpen(false)}>
                 <Login/>
             </Modal>
