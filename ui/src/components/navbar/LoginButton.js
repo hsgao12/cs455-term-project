aa15c671e21405909848fa25af7878b163b3cfad
Joseph <josephg2012@hotmail.com>
2021-07-15 22:29:45 -0700

    Reworked main profile page, added additional styling, linked to backend

--- ui/src/components/navbar/LoginButton.js
+++ ui/src/components/navbar/LoginButton.js
@@ -1,27 +1,20 @@
-import React, {useState} from 'react';
-import {Button, CardHeader, CardMedia, CssBaseline, Input, List, ListItem} from '@material-ui/core';
-import {Container, Grid} from '@material-ui/core';
-import CloseIcon from '@material-ui/icons/Close';
-import {makeStyles, createMuiTheme, ThemeProvider} from '@material-ui/core';
-import {Card} from '@material-ui/core';
-import Login from "../login/Login";
-import Modal from "@material-ui/core/Modal";
-import ContrastNavButton from "./ContrastNavButton";
-
+import React, { useState } from 'react';
+import Login from '../login/Login';
+import Modal from '@material-ui/core/Modal';
+import ContrastNavButton from './ContrastNavButton';
 
 function LoginButton() {
-    const [loginFormOpen, setLoginFormOpen] = useState(false);
-    return <React.Fragment>
-        <ContrastNavButton
-            onClick={()=>setLoginFormOpen(true)}
-            size={"large"}
-        >
-            Login
-        </ContrastNavButton>
-        <Modal open={loginFormOpen} onClose={()=>setLoginFormOpen(false)}>
-            <Login setLoginFormOpen={setLoginFormOpen}/>
-        </Modal>
-    </React.Fragment>;
+  const [loginFormOpen, setLoginFormOpen] = useState(false);
+  return (
+    <React.Fragment>
+      <ContrastNavButton onClick={() => setLoginFormOpen(true)} size={'large'}>
+        Login
+      </ContrastNavButton>
+      <Modal open={loginFormOpen} onClose={() => setLoginFormOpen(false)}>
+        <Login setLoginFormOpen={setLoginFormOpen} />
+      </Modal>
+    </React.Fragment>
+  );
 }
 
 export default LoginButton;
