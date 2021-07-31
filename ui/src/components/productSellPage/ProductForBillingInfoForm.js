5c59f9556c0ee23fe169cc36fb4f1497516416e9
gukaur <gukaur@ciena.com>
2021-07-31 02:21:00 -0700

    error handling

--- ui/src/components/productSellPage/ProductForBillingInfoForm.js
+++ ui/src/components/productSellPage/ProductForBillingInfoForm.js
@@ -7,10 +7,19 @@ import DateFnsUtils from "@date-io/date-fns";
 import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import Grid from "@material-ui/core/Grid";
-import ProductSellConfirmation from "./ProductSellConfirmation";
+import Paper from '@material-ui/core/Paper';
 const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'left',
+    color: 'red',
+    backgroundColor: '#ffcccc'
+  },
   billingInfo: {
     fontWeight: "bold",
+  },
+  bg: {
+   backgroundColor: '#ff7675'
   },
   Info: {
     fontWeight: "bold",
@@ -43,7 +52,9 @@ export default function ProductForBillingInfoForm({
   setBillingData,
 }) {
   const classes = useStyles();
-  const [selectedDate, handleDateChange] = React.useState(new Date());
+  const [error, setError] = React.useState('');
+  const [selectedDate, handleDateChange] = React.useState(null);
+  const [open, setOpen] = React.useState(true);
   const countries = [
     { name: "Canada" },
     { name: "US" },
@@ -52,13 +63,26 @@ export default function ProductForBillingInfoForm({
   ];
 
   const handleSubmit = () => {
-    setConfirmationInfo(true);
     var dt = new Date(selectedDate);
     var dtm = dt.getMonth();
     var dty = dt.getFullYear();
     BillInfo.ExpDate = dtm + "/" + dty;
     setBillingData(BillInfo);
     console.log(BillInfo);
+    if(isNaN(BillInfo.creditCard)){
+      setError("Card Number is not of type Number. Please enter a valid number!");
+    }
+    else if(isNaN(BillInfo.cvv)){
+      setError("CVV is not of type Number. Please enter a valid number!");
+    }
+    else if(!BillInfo.firstName || !BillInfo.lastName || !BillInfo.address ||
+      !BillInfo.province || !BillInfo.postalCode || !BillInfo.country || BillInfo.ExpDate === "11/1969"
+      || !BillInfo.creditCard || !BillInfo.cvv || !BillInfo.ExpDate || BillInfo.ExpDate === null){
+        setError("One of the required fields is empty, please input all the fields!");
+      }
+    else{
+      setConfirmationInfo(true);
+    }
   };
   const handleFirstNameChange = (event) => {
     BillInfo.firstName = event.target.value;
@@ -66,8 +90,14 @@ export default function ProductForBillingInfoForm({
   const handleLastNameChange = (event) => {
     BillInfo.lastName = event.target.value;
   };
-  const handleCountryChange = (event) => {
-    BillInfo.country = countries[event.target.value].name;
+  const handleCountryChange = (event, value) => {
+    if(value === undefined){
+      setError("Please select Country from Drop Down Menu");
+    }
+    else{
+      console.log(value)
+      BillInfo.country = value;
+    }
   };
   const handleAddressChange = (event) => {
     BillInfo.address = event.target.value;
@@ -92,6 +122,10 @@ export default function ProductForBillingInfoForm({
     <div>
       <div className={classes.billingInfo}>Billing</div>
       <div>Please choose your billing method</div>
+      {error!== "" && (
+      <Grid className={classes.bg}item xs={12} sm={12}>
+          <Paper className={classes.paper}>{error}</Paper>
+        </Grid>)}
       <div className={classes.Info}>Credit Card</div>
       <FormControl fullWidth className={classes.margin}>
         <Grid container>
@@ -116,7 +150,7 @@ export default function ProductForBillingInfoForm({
             variant="inline"
             openTo="year"
             minDate={new Date("2021-03-01")}
-            maxDate={new Date("2023-06-01")}
+            maxDate={new Date("2030-06-01")}
             value={selectedDate}
             onChange={(newValue) => {
               handleDateChange(newValue);
@@ -147,13 +181,11 @@ export default function ProductForBillingInfoForm({
           options={countries}
           getOptionLabel={(option) => option.name}
           style={{ width: 300 }}
-          onInputChange={(event) => {
-            handleCountryChange(event);
-          }}
+          onInputChange={handleCountryChange}
           renderInput={(params) => (
             <TextField {...params} label="Country" variant="outlined" />
           )}
-        />{" "}
+        />
         <br></br>
         <TextField
           label="Address"
@@ -199,6 +231,7 @@ export default function ProductForBillingInfoForm({
             Submit
           </Button>
         </Grid>
+      
       </Grid>
     </div>
   );
