b04474911424d0b15e6feac8537ce3d2295aff0c
cerww <calebtran1@hotmail.com>
2021-08-11 20:00:46 -0700

    small fix

--- ui/src/components/productDetailPage/productDetailPage.js
+++ ui/src/components/productDetailPage/productDetailPage.js
@@ -118,10 +118,11 @@ export default function ProductDetailPage(props) {
         </div>
       </div>
       </div>
-        <div style={{gridColumn:"span 2"}}>
-            <Recommended f={true}/>
-        </div>
-   
+          {user && 
+              <div style={{gridColumn: "span 2"}}>
+                  <Recommended f={true}/>
+              </div>
+          }
 
       </React.Fragment>
   );
